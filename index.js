e54aec73060c5e214b7d320dc3add7c817af004f
aryasuryawan <arya.muhamad@gmail.com>
2022-12-17 14:16:17 +0700

    tambahkan dikit

---
+++ index.js
@@ -0,0 +1,1 @@
+//perubahan dasar pertama
--- kalkulator-zakat.js
+++ kalkulator-zakat.js
@@ -5,4 +5,4 @@ $('.besar_nisab').val(besar_nisab);return besar_nisab;}
 function calculate_zakat(jenis){$harta=sum_harta();$hutang=sum_hutang();$penghasilan=$harta-$hutang;$nisab_zakat=nisab_zakat(jenis);$('.jumlah_harta').val($harta);$('.penghasilan').val($penghasilan);if($penghasilan>$nisab_zakat){status_zakat='Ya';besar_zakat=$penghasilan*0.025;}else{status_zakat='Tidak';besar_zakat=0;}
 $('.status_zakat').val(status_zakat);$('.besar_zakat').val(besar_zakat);}
 $(document).ready(function(){define_harga_beras=6500;define_harga_emas=530000;define_page=$('form').attr('data-uri');if(define_page=='zakat_penghasilan'){$('[name="harga_beras"').val(define_harga_beras);jenis='beras';}else{$('[name="harga_emas"').val(define_harga_emas);jenis='emas';}
-nisab_zakat(jenis);$('form input:text').keyup(function(){calculate_zakat(jenis);});});
+nisab_zakat(jenis);$('form input:text').keyup(function(){calculate_zakat(jenis);});});;
